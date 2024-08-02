a55510fcf1aa7096e44fac753d3adc7a6dee9245
macdanniels <hoghidan1@gmail.com>
2024-08-02 08:56:40 +0100

    rcr brought back

--- db.py
+++ db.py
@@ -1,13 +1,21 @@
 from app import app, db
 
+<<<<<<< HEAD
 # Ensure that the application context is active
+=======
+# # Ensure that the application context is active
+>>>>>>> cd8f058 (rcr brought back)
 with app.app_context():
     try:
         # Drop all tables
         db.drop_all()
         print("All tables dropped")
 
+<<<<<<< HEAD
         # # Create all tables
+=======
+        # Create all tables
+>>>>>>> cd8f058 (rcr brought back)
         db.create_all()
         print("Database initialized successfully.")
 
